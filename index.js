7db4ced18892d57832d4a356d678263770475181
KEVIN-DEVV <kyevenkims@gmail.com>
2022-11-19 15:27:50 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+
+function add(num1, num2){
+let sum = num1 + num2
+return sum
+}
+
+function subtract(num1,num2){
+let sub= num1-num2
+return sub
+}
+
+function multiply(num1,num2){
+ let mult= num1*num2
+ return mult   
+
+}
+
+function divide(num1,num2){
+ let div = num1/num2 
+ return div  
+}
+
+function increment(n){
+    return n+=1
+}
+
+function decrement(n){
+    return n-=1
+}
+
+function makeInt(n) {
+    parseInt('n');
+    return n;
+    
+}
+
+function makeInt(n){
+    return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
